81ec45ac50c3abede121af7c2463ae05a0983493
D M <gs9600@gmail.com>
2013-08-28 21:42:14 -0400

    fixed error catching to only catch ENOENT

--- lib/magic_server.rb
+++ lib/magic_server.rb
@@ -11,105 +11,106 @@ include MagicServer
 
 module MagicServer
 
-   class Server
-      attr_accessor :port, :host, :servlets
+  class Server
+    attr_accessor :port, :host, :servlets
 
-      # args is an array passed in from run.rb
-      def initialize(args)
-         @command = args[0]
-         @port = 3333
-         @host = '127.0.0.1'
-         @servlets = {}
-         if args[0].is_a?(String)
-            @host = args[1] if args[0].include? 'h'
-            @port = args[1] if args[0].include? "p"
-         end 
-         @logger = LoggerUtil.instance
+    # args is an array passed in from run.rb
+    def initialize(args)
+      @command = args[0]
+      @port = 3333
+      @host = '127.0.0.1'
+      @servlets = {}
+      if args[0].is_a?(String)
+        @host = args[1] if args[0].include? 'h'
+        @port = args[1] if args[0].include? "p"
       end 
-      
-      # This is where the application really begins. This method
-      # creates the 
-      def start
-         puts "Server created at #{@host} and port #{@port}"
+      @logger = LoggerUtil.instance
+    end 
 
-         self.mount_all(MagicServer::BASE_PATH)
+    # This is where the application really begins. This method
+    # creates the 
+    def start
+      puts "Server created at #{@host} and port #{@port}"
 
-         # Create a server loop
-         Socket.tcp_server_loop(@host, @port) do |connection|
-            # parse the entire request into a key/val map
-            parsed_request = MagicServer::parse_http_request(connection)
-            heading = parsed_request['Request-Line']
-            @logger.info(heading)
+      self.mount_all(MagicServer::BASE_PATH)
 
-            # Get the method from the heading
-            method = heading.split(' ')[0]
+      # Create a server loop
+      Socket.tcp_server_loop(@host, @port) do |connection|
+        # parse the entire request into a key/val map
+        parsed_request = MagicServer::parse_http_request(connection)
+        heading = parsed_request['Request-Line']
+        @logger.info(heading)
 
-            # Remove everything except the path from the heading
-            parsed_request = MagicServer::parse_heading(heading, method)
-            route = parsed_request[:route]
-            puts route
-            begin
-               self.route(route, method, connection, parsed_request)
-            rescue => exception
-               puts exception.to_s
-               puts exception.backtrace
-               connection.print "File not found"
-            end
-            connection.close
-         end
+        # Get the method from the heading
+        method = heading.split(' ')[0]
+
+        # Remove everything except the path from the heading
+        parsed_request = MagicServer::parse_heading(heading, method)
+        route = parsed_request[:route]
+        puts route
+        begin
+          self.route(route, method, connection, parsed_request)
+        rescue Errno::ENOENT => e
+          # Catch file not founds
+          puts e.to_s
+          puts e.backtrace
+          connection.print "File not found"
+        end
+        connection.close
+      end
+    end 
+
+    def mount(route, servlet)
+      @servlets[route] = servlet   
+    end 
+
+    #mount all servlets and match them up with their respective routes
+    def mount_all(basepath)
+      routes = {}
+
+      #put all the routes into a map of classname/routes 
+      File.open(basepath + '/routes', 'r') do |file_handle|
+        file_handle.each_line do |line|
+          split_line = line.split('=')
+          #chomp to get rid of the newline
+          routes[split_line[1].chomp] = split_line[0]   
+        end
+      end
+
+      #load all the application servlets
+      Dir[MagicServer::BASE_PATH + '/servlets/*.rb'].each {|file| require file } 
+
+      #mount ALL the servlets!
+      MagicServer::Servlet.descendants.each do |clazz|
+        self.mount(routes[clazz.name], clazz.new)
       end 
+    end 
 
-      def mount(route, servlet)
-         @servlets[route] = servlet   
+    # If the requested route is a static file (like javascript, css, or image)
+    # then the route will be handled in the else condition. If the route is 
+    # found on the servlets map, then handle it with a servlet
+    def route(route, method, session, parsed_request)
+      view = 'File not found'
+      if @servlets.has_key?(route)
+        case method
+        when 'GET'
+          view = @servlets[route].do_GET(session, parsed_request)
+        when 'POST'
+          view = @servlets[route].do_POST(session, parsed_request)
+        else
+        end 
+      elsif route.to_s.empty?
+        @servlets['/'].do_GET(session, parsed_request)
+      else
+        # All static file requests go here
+        content_type = MagicServer::content_type(MagicServer::get_content_type(route))
+        response = '' << HTTP_SUCCESS << content_type << "\n"
+        #found_file = MagicServer::find_file(route).read
+        response << MagicServer::find_file(route).read
+        session.print(response)
       end 
-
-      #mount all servlets and match them up with their respective routes
-      def mount_all(basepath)
-         routes = {}
-         
-         #put all the routes into a map of classname/routes 
-         File.open(basepath + '/routes', 'r') do |file_handle|
-            file_handle.each_line do |line|
-               split_line = line.split('=')
-               #chomp to get rid of the newline
-               routes[split_line[1].chomp] = split_line[0]   
-            end
-         end
-
-         #load all the application servlets
-         Dir[MagicServer::BASE_PATH + '/servlets/*.rb'].each {|file| require file } 
-         
-         #mount ALL the servlets!
-         MagicServer::Servlet.descendants.each do |clazz|
-            self.mount(routes[clazz.name], clazz.new)
-         end 
-      end 
-
-      # If the requested route is a static file (like javascript, css, or image)
-      # then the route will be handled in the else condition. If the route is 
-      # found on the servlets map, then handle it with a servlet
-      def route(route, method, session, parsed_request)
-         view = 'File not found'
-         if @servlets.has_key?(route)
-            case method
-            when 'GET'
-               view = @servlets[route].do_GET(session, parsed_request)
-            when 'POST'
-               view = @servlets[route].do_POST(session, parsed_request)
-            else
-            end 
-         elsif route.to_s.empty?
-            @servlets['/'].do_GET(session, parsed_request)
-         else
-            # All static file requests go here
-            content_type = MagicServer::content_type(MagicServer::get_content_type(route))
-            response = '' << HTTP_SUCCESS << content_type << "\n"
-            #found_file = MagicServer::find_file(route).read
-            response << MagicServer::find_file(route).read
-            session.print(response)
-         end 
-         return view
-      end 
-   end
+      return view
+    end 
+  end
 end 
 
